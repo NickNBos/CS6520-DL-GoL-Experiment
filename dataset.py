36c97e01f8b6202c124d14d7dddf34e147f12509
Nate Gaylinn <nate.gaylinn@gmail.com>
2024-10-04 11:35:18 -0400

    Make randomization more random. Don't ever try rendering videos one at a time.

--- dataset.py
+++ dataset.py
@@ -19,38 +19,36 @@ def random_transform(seed, obj, obj_data):
 
     # Flip
     if coin_flip():
-        obj = np.flip(obj, axis=rng.integers(2))
+        obj = np.flip(obj, axis=0)
+    if coin_flip():
+        obj = np.flip(obj, axis=1)
 
     # Rotate
-    if coin_flip():
-        obj = np.rot90(obj, k=rng.integers(1, 4))
+    obj = np.rot90(obj, k=rng.integers(4))
 
     # Translate
-    if coin_flip():
-        obj = np.roll(obj, rng.integers(1, WORLD_SIZE), axis=0)
-        obj = np.roll(obj, rng.integers(1, WORLD_SIZE), axis=1)
+    obj = np.roll(obj, rng.integers(WORLD_SIZE), axis=0)
+    obj = np.roll(obj, rng.integers(WORLD_SIZE), axis=1)
 
     # Torch needs the object in a contiguous block of memory.
     obj = np.ascontiguousarray(obj)
 
     # Step simulation
     period = obj_data.select('period').item()
-    if period > 1 and coin_flip():
-        # Simulate for some number of steps, then grab the last frame.
-        obj = simulate_one(obj, rng.integers(1, period))[-1]
+    if period > 1:
+        steps = rng.integers(period)
+        if steps > 0:
+            # Simulate for some number of steps, then grab the last frame.
+            obj = simulate_one(obj, steps)[-1]
 
     return obj
 
 
 class GameOfLifeDataset(Dataset):
-    def __init__(self, full_videos=False):
+    def __init__(self):
         self.df = load_catagolue_data().filter(
             (pl.col('period').is_null()) | (pl.col('period') <= MAX_PERIOD)
         )
-
-        # Whether this dataset should could contain videos of Game of Life
-        # simulations rather than images of the inital state.
-        self.full_videos = full_videos
 
     def __len__(self):
         return len(self.df) * VARIATIONS_PER_IMAGE
@@ -64,15 +62,13 @@ class GameOfLifeDataset(Dataset):
         obj = add_padding(decode(obj_data.select('apgcode').item()))
         obj = random_transform(i, obj, obj_data)
 
-        # Render full videos if requested, otherwise return single images.
-        # TODO: This would be much more efficient if batched.
-        if self.full_videos:
-            obj = simulate_one(obj, steps=VIDEO_LEN)
-
         # Return samples from the Dataset for training.
         return {
-            # Image / video data to classify
-            'image': obj,
+            # Initial state of the Game of Life simulation to classify.
+            # NOTE: To work with video data, use a DataLoader to fetch a whole
+            # batch of initial states, then simulate them in one shot before
+            # passing them in as training data.
+            'initial_state': obj,
             # What kind of object is this (still_life, oscillator, spaceship,
             # or other)?
             'category': obj_data.select('category').to_numpy().flatten(),
@@ -91,7 +87,7 @@ if __name__ == '__main__':
     for batch in dataloader:
         for i, (x, y) in enumerate(np.ndindex(5, 5)):
             plt.subplot(5, 5, i + 1)
-            plt.imshow(batch['image'][i].squeeze(), cmap='Greys')
+            plt.imshow(batch['initial_state'][i].squeeze(), cmap='Greys')
             category = CATEGORY_NAMES[batch['category'][i]]
             period = batch['period'][i].item()
             name = TOP_15_NAMES[batch['obj_id'][i]]
