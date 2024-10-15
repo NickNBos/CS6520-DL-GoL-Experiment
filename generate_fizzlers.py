78af021044c90469200f2a41ef6c4834e64a76cc
NickNBos <nnbosley@uvm.edu>
2024-10-14 20:54:22 -0400

    Finished fizzler generation
    Now, only need to push into the dataset

--- generate_fizzlers.py
+++ generate_fizzlers.py
@@ -6,6 +6,7 @@ torch._dynamo.config.suppress_errors = True
 
 import simulate
 
+from decode import Decoder
 # import polars as pl
 # from pathlib import Path
 # import import_data
@@ -13,10 +14,13 @@ import simulate
 
 # OUTPUT_PATH = Path('catagolue.parquet')
 # df = pl.read_parquet(OUTPUT_PATH)
+padder = Decoder()
 
-def make_pattern(size):
+def make_pattern(size = 3):
     # Make a pattern of shape up to size x size
-    potential_pattern = np.random.randint(0,2,(size,size))
+    potential_pattern = np.zeros((size,size))
+    while potential_pattern.sum() == 0:
+        potential_pattern = np.random.randint(0,2,(size,size))
     
     # It is NOT viable to try to get every combination, as that generates
     # 2**(size**2) combinations [Balloons QUICKLY]
@@ -35,6 +39,37 @@ def make_pattern(size):
     print(initial_states)
     print(frames.shape, frames[-1].sum())
     
+    # Crunch out the x and y dims to retrieve frame sums    
+    lifespan = max(np.where(np.sum(np.sum(frames, axis=1), axis=1) > 0)[0]) + 1
+    
+    if frames[-1].sum() == 0:
+        # Add the standard padding to the pattern
+        # Would be better to have this independent of the decoder, but it shouldn't matter
+        return padder.standard_one_pad(potential_pattern), lifespan
+    else:
+        return None, None
+    
+def generate(size = 3, goal = 50, tries = 200):
+    
+    # Max out at some large number if tries is none
+    if tries is None:
+        tries = 65536
+    
+    pattern_list = []
+    
+    for _ in range(tries):
+        pattern, lifespan = make_pattern()
+        if pattern is not None:
+            pattern_list.append([pattern.tolist(), lifespan])
+            
+            # Only need to check this if you've just added a pattern
+            if len(pattern_list) >= goal:
+                # Allow for an early return if the desired number of patterns is met
+                return pattern_list
+            
+    return np.array(pattern_list)
+
+
 if __name__ == '__main__':
-    make_pattern(5)
+    fizzlers = generate(5)
     
